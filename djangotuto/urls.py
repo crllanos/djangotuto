4c1299ad12df3d528e1cfdd2171c6db368b163d9
Cristian Llanos <cllanos@soluinnova.cl>
2019-11-06 14:10:09 -0300

    ADD .gitignore. ADD Admin and models

--- djangotuto/urls.py
+++ djangotuto/urls.py
@@ -20,4 +20,5 @@ from django.urls import include, path
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     path('encuesta/', include('encuesta.rutas')),
+    path('/', include('encuesta.rutas')),
 ]
--- encuesta/admin.py
+++ encuesta/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Question
 
 # Register your models here.
+# Para que el administrador considere este modelo 
+admin.site.register(Question)
